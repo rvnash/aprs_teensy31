a5c1d9188e9b322e4648f7213cbab7dcf3d24ca3
Richard Nash <rvnash@gmail.com>
2015-03-24 19:58:02 -0400

    Update aprs.h

--- aprs.h
+++ aprs.h
@@ -1,6 +1,12 @@
 /*
  * Copyright (C) 2014 by KC3ARY Rich Nash
  * 
+ * Module modified from code by EA5HAV.
+ *
+ */
+
+/* Credit to the trackuino project written by EA5HAV Javi
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -19,28 +25,24 @@
 #ifndef __APRS_H__
 #define __APRS_H__
 
-struct PathAddress {
-	const char *callsign;
-	uint8_t ssid;
+struct PathAddress
+{
+  const char *callsign;
+  uint8_t ssid;
 };
 
-void aprs_setup(const uint16_t txDelay, // ms to transmit ax25 flag
-                const uint8_t pttPin, // Use PTT pin, 0 = do not use PTT
-                const uint16_t pttDelay, // ms to wait after PTT to transmit
-                const uint32_t p_toneLength, // if <> 0 then emit a sub-audio tone (300hz) before packet to trigger VOX
-		const uint32_t p_silenceLength // Length of time (ms) after tone before packet
-                );
+void aprs_setup(const uint16_t preambleFlags, // number of preambleFlags to send, must be at least 1 to frame packet
+    const uint8_t pttPin, // Use PTT pin, 0 = do not use PTT
+    const uint16_t pttDelay, // ms to wait after PTT to transmit
+    const uint32_t toneLength, const uint32_t silenceLength // Emit sub-audio tone before packet to trigger VOX
+    );
 
-void aprs_send(const PathAddress *const paths, const int nPaths,
-               const uint8_t dayOfMonth,
-	       const uint8_t hour,
-	       const uint8_t min,
-               const float lat,
-	       const float lon, // degrees
-               const float altitude, // meters
-               const uint16_t heading, // degrees
-               const float speed,
-               const char symbolTableIndicator,
-               const char symbol,
-               const char *const comment); // Synchronized ... won't return until the entire packet is sent
+void aprs_send(const PathAddress * const paths, const int nPaths,
+    const uint8_t dayOfMonth, const uint8_t hour, const uint8_t min,
+    const float lat,
+    const float lon, // degrees
+    const float altitude, // meters
+    const uint16_t heading, // degrees
+    const float speed, const char symbolTableIndicator, const char symbol,
+    const char * const comment); // Synchronized ... won't return until the entire packet is sent
 #endif
